2a4c40aa8987f3290e5fa1a2fdbf5f08f63b7bed
hnutanahuja <himanshu.nutanahuja@hitachids.com>
2025-09-27 17:22:08 +0530

    Update HomeController.java

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     
     @GetMapping("/")
-    public String index() {
-        return "index.html"watch;
+    public StringString index() {
+        return "index.html";
     }
 }
